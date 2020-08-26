67197d5722d376c6327b3b8a4a6ea4a8a5848beb
Luis Henrique Rocha Bueno <luizbueno1201@gmail.com>
2020-08-26 17:15:16 -0300

    Criação de uma página dinâmica.

--- meuprojeto/enquetes/models.py
+++ meuprojeto/enquetes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Enquete(object):
+    
+    def __init__(self, texto, data_publicacao):
+        self.texto = texto
+        self.data_publicacao = data_publicacao
--- meuprojeto/enquetes/views.py
+++ meuprojeto/enquetes/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-
+from .models import Enquete
 # Create your views here.
 def bemvindo(request):
     return render(request, 'bemvindo.html')
+
+
+def enquete(request, enquete_id):
+    context = {}
+    if enquete_id == 1:
+        context['enquete'] = Enquete("Como usar classes em Python?", '2020-08-26')
+    elif enquete_id == 1:
+        context['enquete'] = Enquete("O que é Django REST Framework", '2020-07-30') 
+    elif enquete_id == 3:
+        context['enquete'] = Enquete("O que é **kwargs?", '2020-08-16')
+    return render(request, 'enquete.html', context)
